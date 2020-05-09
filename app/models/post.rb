f0f8329bd8e7f5d362a483a9fce865d327201e99
pledgedavis <pledgedavis14@gmail.com>
2020-05-08 21:44:51 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true, uniqueness: true 
+    validates :content, presence: true, uniqueness: true,  length: { minimum: 100 }
+     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+      
 end
